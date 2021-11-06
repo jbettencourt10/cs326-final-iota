44665b0d1a2f742c2bef610ae6ee816bd7672071
Bill <bchen434@gmail.com>
2021-11-06 16:05:15 -0400

    title changes when sidebar clicked and moved loading to javascript

--- MyMediaMix.js
+++ MyMediaMix.js
@@ -1,17 +1,50 @@
-
+window.addEventListener('load', loadLandingMediaList);
 
 function landingBooks(){
-  document.getElementById("trendingText").innerHTML = "Trending Books"
+  document.getElementById("trendingText").innerHTML = "Trending Books";
 }
 
 function landingMovies(){
-  document.getElementById("trendingText").innerHTML = "Trending Movies"
+  document.getElementById("trendingText").innerHTML = "Trending Movies";
 }
 
 function landingTVShows(){
-  document.getElementById("trendingText").innerHTML = "Trending TV Shows"
+  document.getElementById("trendingText").innerHTML = "Trending TV Shows";
 }
 
 function landingMusic(){
-  document.getElementById("trendingText").innerHTML = "Trending Music"
+  document.getElementById("trendingText").innerHTML = "Trending Music";
+}
+
+function loadLandingMediaList(){
+  const mediaList = document.getElementById("mediaList");
+  for(let i = 0; i < 5; ++i){
+    const row = document.createElement("div");
+    row.classList.add("row");
+    const image = document.createElement("div");
+    image.classList.add("col");
+
+    const figure = document.createElement("figure");
+    figure.classList.add("figure");
+
+    const img = document.createElement("img");
+    img.src = "placeholder";
+    img.classList.add("figure-img", "img-fluid", "rounded");
+    img.alt = "Image Placeholder";
+
+    figure.appendChild(img);
+    image.appendChild(figure);
+    row.appendChild(image)
+
+    const title = document.createElement("div");
+    title.classList.add("col");
+    title.innerHTML = "Insert Title";
+    row.appendChild(title);
+
+    const rating = document.createElement("div");
+    rating.classList.add("col");
+    rating.innerHTML = "Insert Rating";
+    row.appendChild(rating);
+    mediaList.appendChild(row);
+  }
 }
