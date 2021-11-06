cf133a3f4f672b169e567a2bfc03bb737f4a907c
Bill <bchen434@gmail.com>
2021-11-06 15:34:48 -0400

    title changes when sidebar clicked

---
+++ MyMediaMix.js
@@ -0,0 +1,17 @@
+
+
+function landingBooks(){
+  document.getElementById("trendingText").innerHTML = "Trending Books"
+}
+
+function landingMovies(){
+  document.getElementById("trendingText").innerHTML = "Trending Movies"
+}
+
+function landingTVShows(){
+  document.getElementById("trendingText").innerHTML = "Trending TV Shows"
+}
+
+function landingMusic(){
+  document.getElementById("trendingText").innerHTML = "Trending Music"
+}
