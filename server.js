c1ad16302eca415ed766e6ec87bdf5a12255f69c
jbettencourt10 <jbettencourt10@gmail.com>
2021-11-06 21:34:10 -0400

    Additions on server.js

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import faker from 'faker';
 import { getIMDBSearch } from './client/imdbFunctions.js';
+import fs from 'fs';
 
 const IMDB_API_KEY = 'k_t249l7q8';
 
 const app = express();
+
+// Just for testing, emulates a database
+const testAccount = JSON.parse(fs.readFileSync('./user.json'));
+
+
 
 app.use(express.json()); // lets you handle JSON input
 
 const port = process.env.PORT || 8080;
 
 const datastore = {};
+
 app.use(express.static('client'));
 
 app.get('/', (req, res) => {
@@ -24,10 +31,16 @@ app.get('/signup-page', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
+    // For now, pretend like we are logged in when we click login
+    // This will change once we make a database
     res.sendFile('client/list.html', { root: '.' });
 });
 
 app.post('register', (req, res) => {
+    // Once we have a database, the signup page will route
+    // to list.html after signing up
+    res.sendFile('client/list.html', { root: '.' });
+
     // const emailAddress = req.email;
     // const { name } = req;
     // const { username } = req;
@@ -37,8 +50,15 @@ app.post('register', (req, res) => {
     // }
 });
 
-app.get('/search', (req, res) => {
+app.get('/search', async(req, res) => {
+    const searchResult = await getIMDBSearch(req.query.title, req.query.media);
+    res.sendFile('client/search.html', { root: '.' });
+
+});
+
+app.get('/delete', (req, res) => {
     // const searchResult = await getIMDBSearch(req.query.title, req.query.media);
+    // console.log(searchResult);
     res.sendFile('client/search.html', { root: '.' });
 
 });
