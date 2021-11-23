ba443ce5a48c182ca2cbcfffffc5aec0d3d0cef3
Bill <bchen434@gmail.com>
2021-11-23 00:15:39 -0500

    added search placeholders

--- client/search.js
+++ client/search.js
@@ -1,36 +1,33 @@
-// async function loadSearchList(media) {
-//   document.getElementById('mediaList').innerHTML = '';
-//   const top100Object = await getTopIMDB(media);
-//   const top100Media = top100Object.items.slice(0, 5);
-//   const mediaList = document.getElementById('mediaList');
-//   for (let i = 0; i < 5; ++i) {
-//     const row = document.createElement('div');
-//     row.classList.add('row');
-//     const image = document.createElement('div');
-//     image.classList.add('col');
+function loadSearchList(media) {
+  const searchList = document.getElementById('searchList');
+  searchList.innerHTML = '';
+  for (let i = 0; i < 5; ++i) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    const image = document.createElement('div');
+    image.classList.add('col');
 
-//     const figure = document.createElement('figure');
-//     figure.classList.add('figure');
+    const figure = document.createElement('figure');
+    figure.classList.add('figure');
 
-//     const img = document.createElement('img');
-//     img.width = 100;
-//     img.src = top100Media[i].image;
-//     img.classList.add('figure-img', 'img-fluid', 'rounded');
-//     img.alt = 'Image Placeholder';
+    const img = document.createElement('img');
+    img.width = 100;
+    img.classList.add('figure-img', 'img-fluid', 'rounded');
+    img.alt = 'Image Placeholder';
 
-//     figure.appendChild(img);
-//     image.appendChild(figure);
-//     row.appendChild(image);
+    figure.appendChild(img);
+    image.appendChild(figure);
+    row.appendChild(image);
 
-//     const title = document.createElement('div');
-//     title.classList.add('col');
-//     title.innerHTML = top100Media[i].title;
-//     row.appendChild(title);
+    const title = document.createElement('div');
+    title.classList.add('col');
+    title.innerHTML = "Title Placeholder";
+    row.appendChild(title);
 
-//     const rating = document.createElement('div');
-//     rating.classList.add('col');
-//     rating.innerHTML = top100Media[i].imDbRating;
-//     row.appendChild(rating);
-//     mediaList.appendChild(row);
-//   }
-// }
+    const rating = document.createElement('div');
+    rating.classList.add('col');
+    rating.innerHTML = "Rating Placeholder";
+    row.appendChild(rating);
+    searchList.appendChild(row);
+  }
+}
