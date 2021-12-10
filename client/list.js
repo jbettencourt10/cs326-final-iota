781de7b797329dea679c216451150069bc264878
Aimee <aimee.trinh3010@gmail.com>
2021-12-10 05:39:17 -0500

    complete list.js

--- client/list.js
+++ client/list.js
@@ -47,16 +47,9 @@ async function generateList(mediaType){
  * @param {int} direction 
  */
 async function shiftList(mediaType, list, listName, listIndex, direction){
-  if(direction === -1 && listIndex === 0){
-    console.log("Cannot shift left at the beginning of the list");
-  }
-  console.log("In shift")
   const listLength = list.length;
-  console.log(listLength);
   const start = listIndex;
   const end = listIndex + 5;
-  console.log(start);
-  console.log(end);
   if (direction === 1){
     //move right
     if (listName !== "top"){
@@ -72,7 +65,6 @@ async function shiftList(mediaType, list, listName, listIndex, direction){
     }
     else{
       if (end + 5 <= listLength){
-        console.log("in here");
         loadTrendingList(mediaType, list, listIndex+5);
       }
       else if (listLength - 5 >= 0){
@@ -227,8 +219,6 @@ async function loadTrendingList(mediaType, list, listIndex){
 
 async function loadLists(mediaType, list, listName, listIndex){
   const mediaList = document.getElementById(listName);
-  console.log(listName);
-  console.log(mediaList);
   mediaList.innerHTML = '';
   const listLength = Math.min(5, list.length);
   const leftArrowContainer = document.createElement('div');
